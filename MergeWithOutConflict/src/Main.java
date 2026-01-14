a5ade16905922da1d517d5d86259976edd801e5c
esfiio <diegodanchez24@gmail.com>
2026-01-13 21:57:33 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("Semana 2");
         System.out.println("Versión base");
+
+        System.out.println("2+2 = 4");
+        System.out.println("5*3 = 15");
 
     }
 }
