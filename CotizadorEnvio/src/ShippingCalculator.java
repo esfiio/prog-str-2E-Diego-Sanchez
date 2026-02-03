0cd924c2ff40672564b785bf00abd1d49bebe237
esfiio <diegodanchez24@gmail.com>
2026-02-03 14:19:04 -0600

    Avance de actividad

---
+++ CotizadorEnvio/src/ShippingCalculator.java
@@ -0,0 +1,11 @@
+public class ShippingCalculator {
+    public double IVA=0.16;
+    public double pesoMinimo = 0.1;
+    public double pesoMaximo = 50.0;
+    public int disctanciaMinima = 1;
+    public int distanciaMaxima = 2000;
+    public int precioEstandar = 50;
+    public int precioExpress = 90;
+    public boolean esZonaRemota;
+
+}
