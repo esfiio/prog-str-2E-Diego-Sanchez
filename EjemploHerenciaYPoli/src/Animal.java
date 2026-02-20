c5211464ea62422ae1db8ab67a6b9702febc0176
Diego Sanchez <diegodanchez24@gmail.com>
2026-02-19 22:17:23 -0600

    Apuntes de herencia y polimorfismo

---
+++ EjemploHerenciaYPoli/src/Animal.java
@@ -0,0 +1,26 @@
+public class Animal {
+
+    private String nombre;
+
+    public Animal(String nombre){
+        this.nombre = nombre;
+    }
+
+
+    public void comer(){
+        System.out.println(nombre+" esta comiendo");
+    }
+
+    public void hacerSonido(){
+        System.out.println(nombre+ " esta haciendo un sonido");
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ EjemploHerenciaYPoli/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Gato pascal = new Gato("Pascal");
+        Perro ubba = new Perro("Ubba");
+
+        pascal.comer();
+        pascal.hacerSonido();
+        System.out.println("-----------------");
+        ubba.hacerSonido();
+        ubba.comer();
+
+
+        Animal[] animales= new Animal[3];
+        animales[0]=new Perro("Rufus");
+        animales[1] = new Gato("Honey");
+        animales[2] = new Perro("Solovino");
+        //Poliformismo
+        for(Animal animal: animales){
+            System.out.println("-------");
+            animal.comer();
+            animal.hacerSonido();
+        }
+
+        //Cast, solo para hacer uso de los metodos de perro, a pesar de que anmal1 es un objeto de Animal
+        Animal animal1= new Perro("nicky");
+        Perro perr1 = (Perro)animal1;
+        perr1.marcarTerritorio();
+
+    }
+}
